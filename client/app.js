6e45af084244ff337152ef64ca2b7162b3d62aba
rf222cz <rf222cz@student.lnu.se>
2016-03-14 19:04:11 +0100

    Sessionstorage on restaurants and tags

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
 'use strict';
-var app = angular.module('RestaurantMap', ['ui.router', 'ui.bootstrap', 'ngMap']);
+var app = angular.module('RestaurantMap', ['ui.router', 'ui.bootstrap', 'ngMap', 'ngStorage']);
 app.constant('api_key', 'fb3737b1c0d01edd92cf64262bc8efdf')
 app.constant('base_url', 'http://localhost:3000/api/')
 
--- client/js/Controllers/RestaurantCtrl.js
+++ client/js/Controllers/RestaurantCtrl.js
@@ -1,48 +1,71 @@
 
-app.controller('RestaurantCtrl', ['$scope', 'RestaurantService', 'TagService',
-function($scope, RestaurantService, TagService) {
+app.controller('RestaurantCtrl', ['$scope', '$sessionStorage', 'RestaurantService', 'TagService',
+function($scope, $sessionStorage, RestaurantService, TagService) {
   'use strict';
 
   $scope.restaurants;
   $scope.status;
   $scope.tag;
 
+//Kolla om det finns något sparat i sessionen, eller om utgångsdaumet för sessionen har gått ut.
+//Hämta nya taggar om sessionen inte stämmer
+//Annars använder vi oss utav de som finns sparade i sessionen
+//taggar
+  if($sessionStorage.tag == undefined || $sessionStorage.tag.expire > new Date().getTime()){
+    getTags();
+  }else{
+    $scope.tag = $sessionStorage.tag.data;
+  }
+//restauranger
+  if($sessionStorage.restaurants == undefined || $sessionStorage.restaurants.expire > new Date().getTime()){
+    getRestaurants();
+  }else{
+    $scope.restaurants = $sessionStorage.restaurants.data;
+  }
 
-  TagService.getTags()
-    .success(function(data){
-      $scope.tag = data.tags;
-      $scope.tag.unshift({name: 'All tags'});
-    })
-    .error(function (error) {
-        //$scope.status = 'Unable to load tags: ' + error.message;
-        console.log('Unable to load tags: ' + error);
-    });
-    $scope.searchTag = function(){
-      getRestaurantbyTag(this.select.id);
-    }
 
-  RestaurantService.getRestaurants()
-          .success(function (data) {
-              $scope.restaurants = data.restaurants;
-          })
-          .error(function (error) {
-              //$scope.status = 'Unable to load restaurant data: ' + error.message;
-              console.log('Unable to load restaurant data: ' + error);
-          });
-
+//Vid förändring i select-tags
+  $scope.searchTag = function(){
+    getRestaurantbyTag(this.select.id);
+  }
 
 
 
-
- function getRestaurantbyTag(id){
-    RestaurantService.getRestaurantbyTag(id)
+//Hämtar alla taggar
+  function getTags(){
+    TagService.getTags()
+      .success(function(data){
+        $scope.tag = data.tags;
+        $scope.tag.unshift({name: 'All tags'});
+        $sessionStorage.tag = {data: $scope.tag, expire: new Date().getTime()+(1*60000)};
+      })
+      .error(function (error) {
+          //$scope.status = 'Unable to load tags: ' + error.message;
+          console.log('Unable to load tags: ' + error);
+      });
+  }
+//Hämtar alla restauranger
+  function getRestaurants(){
+    RestaurantService.getRestaurants()
             .success(function (data) {
                 $scope.restaurants = data.restaurants;
+                $sessionStorage.restaurants = {data: $scope.restaurants, expire: new Date().getTime()+(1*60000)};
             })
             .error(function (error) {
-                //$scope.status = 'Unable to load restaurant by tag data: ' + error.message;
-                console.log('Unable to load restaurant by tag data: ' + error.message);
-            });;
+                //$scope.status = 'Unable to load restaurant data: ' + error.message;
+                console.log('Unable to load restaurant data: ' + error);
+            });
+  }
+//Hämtar alla restauranger med viss tag
+ function getRestaurantbyTag(id){
+   RestaurantService.getRestaurantbyTag(id)
+           .success(function (data) {
+               $scope.restaurants = data.restaurants;
+           })
+           .error(function (error) {
+               //$scope.status = 'Unable to load restaurant by tag data: ' + error.message;
+               console.log('Unable to load restaurant by tag data: ' + error.message);
+           });
   }
 
 }]);

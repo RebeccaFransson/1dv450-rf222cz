4400f382518b39cadd960fdeb40dbeacf44faf86
rf222cz <rf222cz@student.lnu.se>
2016-03-17 14:54:12 +0100

    Delete restaurants

--- client/js/Controllers/CreatorRestaurantsCtrl.js
+++ client/js/Controllers/CreatorRestaurantsCtrl.js
@@ -1,25 +1,28 @@
 app.controller('CreatorRestaurantsCtrl', ['$scope', '$stateParams', 'RestaurantService', 'AlertService', 'LoginService',
 function($scope, $stateParams, RestaurantService, AlertService, LoginService){
+getRestaurants();
 
+function getRestaurants(){
   RestaurantService.getRestaurantbyCreator($stateParams.id)
         .success(function(data){
           console.log(data);
           $scope.restaurants = data.restaurants;
         })
         .error(function (error) {
-            AlertService.handlesAlerts(false, 'someting went wrong getting the restaurants', 'danger');
+            AlertService.handlesAlerts(false, 'Someting went wrong when getting the restaurants', 'danger');
         });
+}
+
 
   $scope.deleteRestaurant = function(id){
     console.log(LoginService.isLoggedIn().token.jwt);
     RestaurantService.deleteRestaurant(id, LoginService.isLoggedIn().token.jwt)
           .success(function(data){
-            console.log(data);
-            //$scope.restaurants = data.restaurants;
-            AlertService.handlesAlerts(true, 'removed', 'warning');
+            AlertService.handlesAlerts(true, data.message, 'warning');
+            getRestaurants();
           })
           .error(function (error) {
-            AlertService.handlesAlerts(false, 'someting went wrong getting the restaurants', 'danger');
+            AlertService.handlesAlerts(false, 'Someting went wrong, try again', 'danger');
           });
   }
 }]);
--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Lab1dv411
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
           origins '*'
-          resource '*', :headers => :any, :methods => [:get, :post, :options]
+          resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
         end
     end
   end
